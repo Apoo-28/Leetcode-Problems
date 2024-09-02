59c5c40c554d4d1a1013e9ff5f7c10429c903d06
Apoo-28 <153870198+Apoo-28@users.noreply.github.com>
2024-09-02 17:41:46 +0530

    Time: 2 ms (72.59%), Space: 59.8 MB (68.19%) - LeetHub

---
+++ 2006-find-the-student-that-will-replace-the-chalk/2006-find-the-student-that-will-replace-the-chalk.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int chalkReplacer(int[] chalk, int k) {
+        long s = 0;
+        for (int x : chalk) {
+            s += x;
+        }
+        k %= s;
+        for (int i = 0;; ++i) {
+            if (k < chalk[i]) {
+                return i;
+            }
+            k -= chalk[i];
+        }
+    
+    }
+}
