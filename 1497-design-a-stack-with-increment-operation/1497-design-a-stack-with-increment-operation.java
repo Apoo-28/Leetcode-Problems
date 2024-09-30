d50db23b3d50d84b412e892d350df07ddb14ce62
Apoo-28 <153870198+Apoo-28@users.noreply.github.com>
2024-09-30 20:08:10 +0530

    Time: 4 ms (100%), Space: 45.8 MB (35.82%) - LeetHub

---
+++ 1497-design-a-stack-with-increment-operation/1497-design-a-stack-with-increment-operation.java
@@ -0,0 +1,35 @@
+class CustomStack {
+    private int[] stk;
+    private int[] add;
+    private int i;
+
+    public CustomStack(int maxSize) {
+        stk = new int[maxSize];
+        add = new int[maxSize];
+    }
+
+    public void push(int x) {
+        if (i < stk.length) {
+            stk[i++] = x;
+        }
+    }
+
+    public int pop() {
+        if (i <= 0) {
+            return -1;
+        }
+        int ans = stk[--i] + add[i];
+        if (i > 0) {
+            add[i - 1] += add[i];
+        }
+        add[i] = 0;
+        return ans;
+    }
+
+    public void increment(int k, int val) {
+        if (i > 0) {
+            add[Math.min(i, k) - 1] += val;
+        }
+    }
+}
+
