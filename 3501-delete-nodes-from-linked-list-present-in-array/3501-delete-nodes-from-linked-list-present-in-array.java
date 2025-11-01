dbd7ce2215d341ebad9458652c06cce33dbb114d
Apoorva Aanand <153870198+Apoo-28@users.noreply.github.com>
2025-11-01 21:39:55 +0530

    Time: 31 ms (5.29%), Space: 147.3 MB (5.52%) - LeetHub

--- 3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.java
+++ 3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.java
@@ -1,27 +1,14 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
-    public ListNode modifiedList(int[] nums, ListNode head) {
-         Set<Integer> s = new HashSet<>();
-        for (int x : nums) {
-            s.add(x);
-        }
-        ListNode dummy = new ListNode(0, head);
-        for (ListNode pre = dummy; pre.next != null;) {
-            if (s.contains(pre.next.val)) {
-                pre.next = pre.next.next;
-            } else {
-                pre = pre.next;
-            }
-        }
-        return dummy.next;
-    }
+  public ListNode modifiedList(int[] nums, ListNode head) {
+    ListNode dummy = new ListNode(0, head);
+    Set<Integer> numsSet = Arrays.stream(nums).boxed().collect(Collectors.toSet());
+
+    for (ListNode curr = dummy; curr.next != null;)
+      if (numsSet.contains(curr.next.val))
+        curr.next = curr.next.next;
+      else
+        curr = curr.next;
+
+    return dummy.next;
+  }
 }
