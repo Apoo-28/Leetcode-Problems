26841c1c62d4670b70c417444859539e1fbff83c
Apoorva Aanand <153870198+Apoo-28@users.noreply.github.com>
2025-11-02 20:50:16 +0530

    Time: 46 ms (15.33%), Space: 166.4 MB (5.11%) - LeetHub

--- 2343-count-unguarded-cells-in-the-grid/2343-count-unguarded-cells-in-the-grid.java
+++ 2343-count-unguarded-cells-in-the-grid/2343-count-unguarded-cells-in-the-grid.java
@@ -1,32 +1,54 @@
 class Solution {
-    public int countUnguarded(int m, int n, int[][] guards, int[][] walls) {
-        int[][] g = new int[m][n];
-        for (var e : guards) {
-            g[e[0]][e[1]] = 2;
-        }
-        for (var e : walls) {
-            g[e[0]][e[1]] = 2;
-        }
-        int[] dirs = {-1, 0, 1, 0, -1};
-        for (var e : guards) {
-            for (int k = 0; k < 4; ++k) {
-                int x = e[0], y = e[1];
-                int a = dirs[k], b = dirs[k + 1];
-                while (x + a >= 0 && x + a < m && y + b >= 0 && y + b < n && g[x + a][y + b] < 2) {
-                    x += a;
-                    y += b;
-                    g[x][y] = 1;
-                }
-            }
-        }
-        int ans = 0;
-        for (var row : g) {
-            for (int v : row) {
-                if (v == 0) {
-                    ++ans;
-                }
-            }
-        }
-        return ans;
+  public int countUnguarded(int m, int n, int[][] guards, int[][] walls) {
+    int ans = 0;
+    char[][] grid = new char[m][n];
+    char[][] left = new char[m][n];
+    char[][] right = new char[m][n];
+    char[][] up = new char[m][n];
+    char[][] down = new char[m][n];
+
+    for (int[] guard : guards)
+      grid[guard[0]][guard[1]] = 'G';
+
+    for (int[] wall : walls)
+      grid[wall[0]][wall[1]] = 'W';
+
+    for (int i = 0; i < m; ++i) {
+      char lastCell = 0;
+      for (int j = 0; j < n; ++j)
+        if (grid[i][j] == 'G' || grid[i][j] == 'W')
+          lastCell = grid[i][j];
+        else
+          left[i][j] = lastCell;
+      lastCell = 0;
+      for (int j = n - 1; j >= 0; --j)
+        if (grid[i][j] == 'G' || grid[i][j] == 'W')
+          lastCell = grid[i][j];
+        else
+          right[i][j] = lastCell;
     }
+
+    for (int j = 0; j < n; ++j) {
+      char lastCell = 0;
+      for (int i = 0; i < m; ++i)
+        if (grid[i][j] == 'G' || grid[i][j] == 'W')
+          lastCell = grid[i][j];
+        else
+          up[i][j] = lastCell;
+      lastCell = 0;
+      for (int i = m - 1; i >= 0; --i)
+        if (grid[i][j] == 'G' || grid[i][j] == 'W')
+          lastCell = grid[i][j];
+        else
+          down[i][j] = lastCell;
+    }
+
+    for (int i = 0; i < m; ++i)
+      for (int j = 0; j < n; ++j)
+        if (grid[i][j] == 0 && left[i][j] != 'G' && right[i][j] != 'G' && up[i][j] != 'G' &&
+            down[i][j] != 'G')
+          ++ans;
+
+    return ans;
+  }
 }
